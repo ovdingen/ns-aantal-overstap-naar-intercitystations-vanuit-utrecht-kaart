7759fe816728a1c8755bf438a87208b2a8063282
Hillebrand van de Groep <github@hillebrand.io>
2018-04-09 21:09:25 +0200

    updated import script to include positions of stations

--- station-import/import.php
+++ station-import/import.php
@@ -9,6 +9,8 @@ CREATE TABLE nsstations (
 "stationcode" VARCHAR(8),
 "stationname" VARCHAR(255),
 "station_type" VARCHAR(64),
+"station_lat" FLOAT,
+"station_lon" FLOAT,
 PRIMARY KEY("stationcode")
 );
 EOF;
@@ -22,7 +24,7 @@ $dropquery->execute();
 $createquery = $db->prepare($db_schema);
 $createquery->execute(); // Clears the database. Comment this if you don't want to clear the table. I can't see why you'd want that, though.
 
-$insertquery = $db->prepare("INSERT INTO nsstations VALUES(:stationcode, :stationname, :station_type)"); // Prepare the SQL statement. A prepared statement can be reused infinitely, so we're going to make use of this so we don't need to create a prepared statement for every train station.
+$insertquery = $db->prepare("INSERT INTO nsstations VALUES(:stationcode, :stationname, :station_type, :station_lat, :station_lon)"); // Prepare the SQL statement. A prepared statement can be reused infinitely, so we're going to make use of this so we don't need to create a prepared statement for every train station.
 
 foreach($xml as $k => $v) {
     if($v->Land != "NL") {
@@ -33,6 +35,8 @@ foreach($xml as $k => $v) {
     $data['stationcode'] = (string) $v->Code;
     $data['stationname'] = (string) $v->Namen->Lang; // The XML data has 3 names, a short one, a middle-length one, and a long one. There's currently no need for short names.
     $data['station_type'] = (string) $v->Type; // The fact that SimpleXML requires every fucking thing to be casted to a string even when requesting A INDIVIDUAL VALUE is slightly crappy imo. But it's PHP, so that's just the way it is.
+    $data['station_lat'] = (float) $v->Lat;
+    $data['station_lon'] = (float) $v->Lon;
     // TODO: Misschien ook toevoegen dat de Friese stationsnamen er ook in worden gezet, die staan er als alias in en we willen niet de 10 mensen die Fries spreken tegen het been stoten /sarcasm off
     $insertquery->execute($data);
 }
